2c5106760a51df8b20d8da70346ed843b291ce15
Stuart Myers <LaustinSpayce@users.noreply.github.com>
2023-01-21 23:10:54 +0800

    Add socket to the chat box

--- src/game/elements/chatBox/ChatBox.tsx
+++ src/game/elements/chatBox/ChatBox.tsx
@@ -1,12 +1,17 @@
-import React, { useEffect, useRef } from 'react';
+import React, { useEffect, useRef, useState } from 'react';
 import { useAppSelector } from '../../../app/Hooks';
 import { RootState } from '../../../app/Store';
 import ChatInput from '../chatInput/ChatInput';
 import styles from './ChatBox.module.css';
+import io from 'socket.io-client';
+
+const socket = io('/socket');
 
 export default function ChatBox() {
   const chatLog = useAppSelector((state: RootState) => state.game.chatLog);
   const messagesEndRef = useRef<HTMLDivElement>(null);
+  const [isConnected, setIsConnected] = useState(socket.connected);
+  const [lastPong, setLastPong] = useState(null);
 
   const scrollToBottom = () => {
     messagesEndRef.current?.scrollIntoView({
@@ -18,6 +23,31 @@ export default function ChatBox() {
   useEffect(() => {
     scrollToBottom();
   }, [chatLog]);
+
+  useEffect(() => {
+    // this is a beta! Do not use in production builds!
+    if (import.meta.env.PROD) {
+      return;
+    }
+
+    socket.on('connect', () => {
+      setIsConnected(true);
+    });
+
+    socket.on('disconnect', () => {
+      setIsConnected(false);
+    });
+
+    socket.on('pong', () => {
+      console.log('pong');
+    });
+
+    return () => {
+      socket.off('connect');
+      socket.off('disconnect');
+      socket.off('pong');
+    };
+  }, []);
 
   // TODO We really should not be dangerouslySetInnerHTML it's pretty bad mmkay
   return (
